179549f614942f06cc74dbae17ec33c71df37b71
tca85 <tca85@tcantb>
2016-04-28 13:15:53 -0400

    some small adjusts

--- app/src/main/java/com/github/tca85/loginapp/LoginActivity.java
+++ app/src/main/java/com/github/tca85/loginapp/LoginActivity.java
@@ -2,13 +2,17 @@ package com.github.tca85.loginapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+/**
+ * tca85
+ */
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
     }
 }
